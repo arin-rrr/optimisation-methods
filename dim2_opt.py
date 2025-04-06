fbdd4b65483d60c08f584594e9bfb8dc1448a377
Arina <98526242+arin-rrr@users.noreply.github.com>
2025-04-06 14:35:19 +0300

    Add files via upload

--- dim2_opt.py
+++ dim2_opt.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from methods_dim2 import coord_descent
+from methods_dim2 import coord_descent, grad_descent_step_split, mngs, fast_grad_p, ovr_method
 
 
 def my_func2(x1, x2):
     return x1 + 2 * x2 + 4 * (1 + x1 ** 2 + x2 ** 2) ** 0.5
+
+
+def df2(x1, x2):
+    return [1 + 4 * x1 / (1 + x1 ** 2 + x2 ** 2) ** 0.5, 2 + 4 * x2 / (1 + x1 ** 2 + x2 ** 2) ** 0.5]
+
 
 x = np.linspace(-7, 7, 200)
 y = np.linspace(-7, 7, 200)
@@ -28,6 +33,28 @@ print()
 print("Метод покоординатного спуска")
 print(f"x_min = {x1_min_desc}, погрешность = {eps}")
 print(f"f(x_min) = {y1_min_desc}")
+print()
 
+x1_min_split_step = grad_descent_step_split(x0, my_func2, df2)
+print("Градиентный спуск с дроблением шага")
+print(f"x_min = {x1_min_split_step}")
+print(f"f(x_min) = {my_func2(x1_min_split_step[0], x1_min_split_step[1])}")
 
+print()
+x1_min_mngs = mngs(x0, my_func2, df2)
+print("МНГС")
+print(f"x_min={x1_min_mngs}")
+print(f"f(x_min) = {my_func2(x1_min_mngs[0], x1_min_mngs[1])}")
+
+x1_min_p = fast_grad_p(x0, my_func2, df2)
+print()
+print("Ускоренный градиентный метод p-ого порядка")
+print(f"x_min={x1_min_p}")
+print(f"f(x_min) = {my_func2(x1_min_p[0], x1_min_p[1])}")
+
+x_min_ovr = ovr_method(x0, my_func2, df2)
+print()
+print("Овражный метод")
+print(f"x_min={x_min_ovr}")
+print(f"f(x_min) = {my_func2(x_min_ovr[0], x_min_ovr[1])}")
 plt.show()
--- methods_dim2.py
+++ methods_dim2.py
@@ -21,3 +21,119 @@ def coord_descent(x0, eps, func):
             if k >= 5:
                 break
     return x, func(x[0], x[1])
+
+
+def grad_descent_step_split(x0, func, df, alpha=1, eps=10 ^ (-6), lam=0.1, sigma=0.1):
+    x = np.array(x0)
+
+    for _ in range(1000):
+        # считаю градиент в существующей точке
+        gradient = np.array(df(x[0], x[1]))
+
+        # вдруг в начальной точке выполнен критерий остановки
+        if np.linalg.norm(gradient) < eps:
+            break
+
+        # выполняем дроблемние шага
+        while True:
+            x_new = x - alpha * gradient
+            if func(x_new[0], x_new[1]) - func(x[0], x[1]) <= -sigma * alpha * np.linalg.norm(df(x[0], x[1])) ** 2:
+                break
+            alpha *= lam
+
+        x = x_new
+    return x
+
+
+def mngs(x0, func, df, eps=0.001, fast_grad=False):
+    x = np.array(x0, dtype=np.float64)
+    if not fast_grad:
+        for _ in range(1000):
+            gradient = np.array(df(x[0], x[1]), dtype=np.float64)
+
+            # проверка критерий остановки
+            if np.linalg.norm(gradient) <= eps:
+                break
+
+            # функция для нахождения оптимального альфа
+            def f_min_alpha(alpha):
+                x_new = x - alpha * gradient
+                return func(x_new[0], x_new[1])
+
+            alpha_opt = golden_ratio(0, 1, 0.1, f_min_alpha)
+
+            x -= alpha_opt[0] * gradient
+        return x
+
+    else:
+        for _ in range(2):
+            gradient = np.array(df(x[0], x[1]), dtype=np.float64)
+
+            # проверка критерий остановки
+            if np.linalg.norm(gradient) <= eps:
+                break
+
+            # функция для нахождения оптимального альфа
+            def f_min_alpha(alpha):
+                x_new = x - alpha * gradient
+                return func(x_new[0], x_new[1])
+
+            alpha_opt = golden_ratio(0, 1, 0.1, f_min_alpha)
+
+            x -= alpha_opt[0] * gradient
+
+        return x
+
+
+def fast_grad_p(x_begin, func, df, eps=10**(-6)):
+    x = np.array(x_begin)
+
+    for _ in range(1000):
+        y0 = mngs(x, func, df, fast_grad=True)
+
+        if np.linalg.norm(df(x[0], x[1])) <= eps:
+            break
+
+        # определяем направление
+        direction = y0 - x
+
+        def f_min_alpha(alpha):
+            return func(x[0] + alpha * direction[0], x[1] + alpha * direction[1])
+
+        alpha_opt = golden_ratio(0, 1, 0.1, f_min_alpha)[0]
+
+        x = x + alpha_opt * direction
+    return x
+
+
+def ovr_method(x_begin, func, df, delta=0.1, eps=10**(-4)):
+    x = np.array(x_begin)
+
+    for _ in range(1000):
+        x_tilde = x + delta * np.random.randn(2)
+
+        def grad_step(x):
+            gradient = np.array(df(x[0], x[1]))
+
+            def f_min_alpha(alpha):
+                return func(x[0] - alpha * gradient[0], x[1] - alpha * gradient[1])
+
+            alpha_opt = golden_ratio(0, 1, 0.1, f_min_alpha)[0]
+            return x - alpha_opt * gradient
+
+        y = grad_step(x)
+        y_tilde = grad_step(x_tilde)
+
+        direction = y_tilde - y
+
+        # минимизируем вдоль направления
+        def f_alpha(alpha):
+            return func(y[0] + alpha * direction[0], y[1] + alpha * direction[1])
+
+        alpha_opt = golden_ratio(0, 1, 0.01, f_alpha)[0]
+        x_new = y + alpha_opt*direction
+
+        if np.linalg.norm(df(x_new[0], x_new[1])) <= eps:
+            return x_new
+        x = x_new
+    # return x
